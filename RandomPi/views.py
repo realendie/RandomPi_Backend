e6c0cf4abc4d6988f4fb026e19bfedaf9a584146
realendie <enderprooffical@gmail.com>
2025-09-20 21:31:44 -0400

    Added basic code from old RandomPi package

--- RandomPi/views.py
+++ RandomPi/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+import random
 
 # Create your views here.
+
+
+def random_package():
+    url = "https://pypi.org/simple/"
+    headers = {"Accept": "application/vnd.pypi.simple.v1+html"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        projects = response.text
+
+        soup = BeautifulSoup(projects, "html.parser")
+        project_links = soup.find_all("a")
+        project_names = [link.text for link in project_links]
+
+        if project_names:
+            random_project = random.choice(project_names)
+            print(f"pypi.org/project/{random_project}/")
+        else:
+            print("No projects found.")
+    else:
+        print("Failed to fetch projects:", response.status_code, response.text)
